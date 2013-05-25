e80137409e469abe901ca2c143ad7092ed2fce2c
Bertrand Vosse <ber.vosse@gmail.com>
2013-05-25 12:38:42 +0200

    Initial Deposit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7b9c005729323d38cb36788ec8cb5a1a0b1a1ab16bb55a162cec50d60b510352414939f1d96305d507daeb75ad9b61539bc71d4df6d000d52090f79819349480'
